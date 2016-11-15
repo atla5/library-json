a07b51c52f01ee3d1c143ac37ba7fc493a89ef09
Aidan Sawyer <aks5238@rit.edu>
2016-11-15 14:12:30 -0500

    add initial file structure and blank interface

---
+++ src/access/Accessible.java
@@ -0,0 +1,14 @@
+package access;
+
+/**
+ * Interface of getters and setters, to be implemented by each standard
+ *   in order to afford flexibility in process of interoperability.
+ */
+public interface Accessible{
+
+    /* - GETTERS - */
+    // ...
+
+    /* - SETTERS - */
+    // ...
+}
