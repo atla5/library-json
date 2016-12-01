3d79c5f5dbd3679317be0624a34d587764c490fe
Aidan Sawyer <aks5238@rit.edu>
2016-12-01 14:12:06 -0500

    add Aggregate skeleton

---
+++ src/Aggregate.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Representative of the base "Object" in the formal ontology.
+ * - Largest class object to which all accessibles attach
+ * - Mediating object for inter-standard operability and communication.
+ */
+public class Aggregate {
+
+    ArrayList<Accessible> lsAccessibles = new ArrayList<Accessible>();
+
+}
